bbfed15ca64c93eb99e7fb8f6c960542321de6e4
anonymous298 <tackletalha@gmail.com>
2025-09-01 03:50:07 +0500

    footer component created

---
+++ components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='bg-[#556B2F] p-4 text-white font-bold text-center'>
+      &copy; HopLink All Rights Reserved | Made With &hearts; By TALHA
+    </div>
+  )
+}
+
+export default Footer
