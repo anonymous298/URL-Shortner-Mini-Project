37a4e145f79a7d28294235ab8719c8b79e3a1d9a
anonymous298 <tackletalha@gmail.com>
2025-09-01 13:20:30 +0500

    Redirect component created

---
+++ components/Redirect.js
@@ -0,0 +1,17 @@
+"use client"
+
+import React from 'react'
+import { useRouter } from 'next/navigation'
+
+const Redirect = ({fullurl}) => {
+    const router = useRouter();
+
+    router.push(fullurl)
+  return (
+    <div>
+      Redirecting to URL...
+    </div>
+  )
+}
+
+export default Redirect
