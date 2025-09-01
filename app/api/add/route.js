6ca7ec4289835a0e5fcaf955f1e102b2d10bf4ad
anonymous298 <tackletalha@gmail.com>
2025-09-01 13:23:46 +0500

    api route created for db interaction

---
+++ app/api/add/route.js
@@ -0,0 +1,31 @@
+import clientPromise from "@/lib/connectDB";
+import { NextResponse } from "next/server";
+
+
+export async function GET(request) {
+    try {
+        const client = await clientPromise;
+        const db = client.db("urlshortner")
+        const collection = db.collection("urls")
+
+        const data = await collection.find({}).toArray();
+        
+        return NextResponse.json({ data });
+
+    } catch (error) {
+        return NextResponse.json({ error: error.message }, { status: 500 });
+    }
+}
+
+export async function POST(request) {
+    const data = await request.json()
+
+    const client = await clientPromise;
+    const db = client.db("urlshortner")
+    const collection = db.collection("urls")
+
+    await collection.insertOne(data)
+
+    return NextResponse.json({succes : 'true', data : data})
+
+}
